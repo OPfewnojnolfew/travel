277908b19d39745a8c1bb712c4e63cf0b5726efd
ace <34365921@qq.com>
2014-03-06 22:32:40 +0800

    用户页面和弹框

--- js/travel.js
+++ js/travel.js
@@ -1,19 +1,15 @@
 $(function() {
 	$(".isdelete").popover({
 		"html": true,
-		"title": "确定要删除这个活动吗",
+		"title": "确定要删除吗",
 		"placement": "left",
 		"content": function() {
+			var that = this;
 			var $dv = $('<div class="row">' + '<div class="col-md-3 col-md-offset-1"><button type="button" class="btn btn-primary btn-sm">确定</button></div>' + '<div class="col-md-3"><button type="button" class="btn btn-default btn-sm" style="margin-left:10px">取消</button></div>' + '</div>');
-			$dv.children().eq(1).click(function(){
-				$(".isdelete").popover('hide');
+			$dv.find("button").eq(1).click(function() {
+				$(that).popover('hide');
 			})
 			return $dv;
-			// var $div = $('<p/>');
-			// var $true = ('');
-			// var $cancel = ('');
-			// $div.append($true).append($cancel);
-			// return $div;
 		}
 	})
 })
