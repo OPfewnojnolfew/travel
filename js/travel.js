37e0f6db932bfac065524a721eb1ed600f2c05b1
ace <343656921@qq.com>
2014-03-08 17:13:53 +0800

    xxx

--- js/travel.js
+++ js/travel.js
@@ -6,8 +6,8 @@ $(function() {
 		"content": function() {
 			var that = this;
 			var $dv = $('<div class="row">' + '<div class="col-md-3 col-md-offset-1"><button type="button" class="btn btn-primary btn-sm">确定</button></div>' + '<div class="col-md-3"><button type="button" class="btn btn-default btn-sm" style="margin-left:10px">取消</button></div>' + '</div>');
-			$dv.find("button").eq(1).click(function() {
-				$(that).popover('hide');
+			$dv.find("button").eq(1).unbind("click").bind("click", function() {
+				$(".isdelete").popover('hide');
 			})
 			return $dv;
 		}
